f0a4ef8e93710e130a1d97974bb8f66a0e1ce4dd
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-02-27 10:49:21 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,12 @@
 module "ec2" {
     source = "../terraform-aws-instance"
+    #project = "roboshop"
+    #environment = "dev"
     ami_id = "ami-0220d79f3f480ecf5"
-    instance_type = "t3.large"
+    sg_ids = ["sg-028a0f9feae2f961e"]
+    instance_type = "t3.micro"
+    tags = {
+        Name = "roboshop-dev-catalogue"
+    }
+    
 }
